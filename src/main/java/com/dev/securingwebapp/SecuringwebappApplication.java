fa7187113252a51b9095c4350bc854f2a8851d88
jvilasbo <joanavilas.boas.externo@ageas.pt>
2023-08-25 10:08:54 +0100

    Initial implementation

---
+++ src/main/java/com/dev/securingwebapp/SecuringwebappApplication.java
@@ -0,0 +1,13 @@
+package com.dev.securingwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/securingwebapp/SecuringwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.securingwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
